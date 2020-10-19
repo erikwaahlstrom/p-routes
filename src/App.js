9a49050547a2701b8359794b51324f5ff3d3fedc
erikos93 <erikwaahlstrom@gmail.com>
2020-10-19 09:13:55 +0200

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import Button from "react-bootstrap/Button";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Button>Button</Button>
+    </div>
+  );
 };
 
 export default App;
